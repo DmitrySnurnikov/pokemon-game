aa3b8e8d07ecc6ed6197fad0583117204b73add5
DmitrySnurnikov <79356992+DmitrySnurnikov@users.noreply.github.com>
2021-09-21 17:37:07 +0300

    end

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -11,9 +11,35 @@ const firebaseConfig = {
   appId: "1:715582202920:web:c76c8e3f928769cc78811d"
 };
 firebase.initializeApp(firebaseConfig);
-export const fire = firebase;
-export const database = fire.database();
 
-export default database;
+class FireBase{
+  constructor(){
+    this.fire = firebase;
+    this.database = this.fire.database();
+  }
+
+  getPokemonSoket = (cb)=>{
+    this.database.ref('pokemons').on('value', (snapshot)=>{
+    cb(snapshot.val())
+  })
+  }
+
+  offPokemonSoket = ()=>{
+    this.database.ref('pokemons').off()
+  }
+
+  getPokemonsOnce = async ()=>{
+    return await this.database.ref('pokemons').once('value').then(snapshot => snapshot.val());
+  }
+  postPokemon = (key, pokemon) =>{
+    this.database.ref(`pokemons/${key}`).set(pokemon);
+  }
+  addPokemon = (data) =>{
+    const newKey = this.database.ref().child('pokemons').push().key;
+      this.database.ref('pokemons/' + newKey).set(data);
+  }
+}
+
+export default FireBase;
 
 
